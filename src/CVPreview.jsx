7f8f18af5a8baf12605c4f852aeff1889be1efac
unknown <adewaletheophilus@gmail.com>
2023-12-12 09:26:49 +0100

    UI adjustment

--- src/CVPreview.jsx
+++ src/CVPreview.jsx
@@ -1,7 +1,6 @@
 import "./Styles/CVPreview.css";
 import PersonalDetailsView from "./Components/PersonalDetails/PersonalDetailsView";
 import EducationSubSectionView from "./Components/Education/EducationSubSectionView";
-import EducationDataView from "./Components/Education/EducationDataView";
 import WorkExperienceView from "./Components/WorkExperience/WorkExperienceView";
 
 function CVPreview({ personalDetails, eduSection, workExperience }) {
@@ -9,17 +8,6 @@ function CVPreview({ personalDetails, eduSection, workExperience }) {
     <div className="cv-preview">
       <section className="cv">
         <PersonalDetailsView {...personalDetails} />
-        {/* {eduSection.map((eduObj) => (
-          <EducationDataView
-            key={eduObj.id}
-            {...eduObj}
-            // institution={eduObj.institution}
-            // degree={eduObj.degree}
-            // start={eduObj.start}
-            // end={eduObj.end}
-            // location={eduObj.location}
-          />
-        ))} */}
         <EducationSubSectionView educationArray={eduSection} />
         <WorkExperienceView workExperienceArray={workExperience} />
       </section>
